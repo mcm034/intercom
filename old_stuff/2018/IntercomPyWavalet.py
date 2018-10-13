4404d5c1539e0e79b96a18451bda40acbed1cd0d
mcm034 <43337140+mcm034@users.noreply.github.com>
2018-10-13 21:46:03 +0200

    Update IntercomPyWavalet.py

--- old_stuff/2018/IntercomPyWavalet.py
+++ old_stuff/2018/IntercomPyWavalet.py
@@ -18,11 +18,9 @@ import time
 RECORD_SECONDS = 5
 FORMAT = pyaudio.paInt16
 CHANNELS = 1
-RATE = 44100
-PORT = 12344
+RATE = 1024
 VALORES = 32
 ITERACIONESDWT = 9
-HOST = "localhost"
 
 
 #Transformada obtiene un array de datos. Realiza la transformada y separa los datos. Cada bloque 32 bits
@@ -109,109 +107,24 @@ def arraySecuencial(data):
         frames.append(data[i])
     return frames
 
-#Enviamos audio usando udp
-#Socket envia audio por puerto e ip indicado
-#Graba sonido y envia
-def enviar(direccionIp, puerto, CHUNK):
-    udpEnviar = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    p = pyaudio.PyAudio()
-    stream = p.open(format=FORMAT,
-                    channels=CHANNELS,
-                    rate=RATE,
-                    input=True,
-                    frames_per_buffer=CHUNK)
-
-    data = stream.read(CHUNK)
-    frames = arraySecuencial(data)
-    diciPlanos = Transformada(frames)
-    print("\nResultado: ")
-    dest = deTransformada(diciPlanos)
-
-    while True:
-        data = stream.read(CHUNK)
-        udpEnviar.sendto(data, (direccionIp, puerto))
-
-
-#Recibimos audio usando udp
-#Socket escucha por el puerto indicado
-#Recibe datos y reproduce
-def recibir(port_receiv, CHUNK):
-    udpRecibir = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    direccion = ('0.0.0.0', port_receiv)
-    udpRecibir.bind(direccion)
-
-    p = pyaudio.PyAudio()
-    stream = p.open(format=FORMAT,
-                    channels=CHANNELS,
-                    rate=RATE,
-                    output=True,
-                    frames_per_buffer=CHUNK)
-
-    while True:
-        data, addr = udpRecibir.recvfrom(CHUNK*2)
-        stream.write(data)
-
-#Calcular tamaño maximo de chunk
-def maxException():
-    chunkPrueba = 1024
-    udpEnviar = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    p = pyaudio.PyAudio()
-    stream = p.open(format=FORMAT,
-                    channels=CHANNELS,
-                    rate=RATE,
-                    input=True,
-                    frames_per_buffer=chunkPrueba)
-    y = 0
-    try:
-        while True:
-            y=y+1
-            data = stream.read(chunkPrueba*y)
-            udpEnviar.sendto(data, ("localhost", 12345))
-            x = chunkPrueba*y
-    except:
-        bit = 1024
-        while x > bit:
-            bit*=2
-        print(bit/2)
-
-#Tamaño del Chunk solicitado mediante teclado
-#Excepcion String a int si hay valor de error
-#Si el tamaño es introducido mal será de 1024
-def tamanoChunk():
-    try:
-        print("Indica el tamaño de Chunk")
-        ChunkSize = int(input())
-    except ValueError:
-        print("Has introducido mal el tamaño. \nUtilizando tamaño 1024")
-        ChunkSize = 1024
-    return ChunkSize
-
-
 #Iniciamos dos hilos: un hilo para reproducir la entrada de udp y otro hilo para enviar grabacion udp
 #Solicitamos direccion IP del host
 def main():
-    print("Calculando el tamaño maximo de chunk")
+   
+    p = pyaudio.PyAudio()
+    stream = p.open(format=FORMAT,
+                    channels=CHANNELS,
+                    rate=RATE,
+                    input=True,
+                    frames_per_buffer=CHUNK)
 
-    maxException()
-
-    time.sleep(0.5)
-
-    CHUNK = tamanoChunk()
-    
-    Tr = Thread(target=recibir, args=(PORT, CHUNK))
-    Tr.daemon = True
-    Tr.start()
-
-    direccionHost = input("Introduce direccion del host: ")
-
-    print("\n***  Grabando")
-
-    Te = Thread(target=enviar, args=(direccionHost, PORT, CHUNK))
-    Te.daemon = True
-    Te.start()
-    Te.join()
-
-
+    while True:
+        data = stream.read(CHUNK)
+        frames = arraySecuencial(data)
+        diciPlanos = Transformada(frames)
+        print("\nResultado: ")
+        dest = deTransformada(diciPlanos)
+        stream.write(data)
 
 if __name__ == '__main__':
     main()
