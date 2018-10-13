af4d0d3e741ef680de607ca4a409935b8e43368c
mcm034 <43337140+mcm034@users.noreply.github.com>
2018-10-13 22:20:24 +0200

    Update IntercomPyWavalet.py

--- old_stuff/2018/IntercomPyWavalet.py
+++ old_stuff/2018/IntercomPyWavalet.py
@@ -120,7 +120,7 @@ def main():
                     frames_per_buffer=CHUNK)
 
     #He puesto el bucle para que se vaya leyendo, aplicando la transformada, detransformada y reproduciendo después
-    #Sin hacer lo de "un único chunk", si no se complicaría bastante. No se si es realmente necesario
+    #Sin hacer lo de "un único chunk", si no se complicaría bastante. No se si esto satisface el issue
     
     while True:
         data = stream.read(CHUNK)
